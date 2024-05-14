3d7dd69733763349a14bc7353cb344a4e7e63124
rooooomania <rooooomania@gmail.com>
2024-05-14 02:34:22 +0000

    Use make_response()

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # app.py
-from flask import Flask
+from flask import Flask, make_response
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!', 200
+    response = make_response('hello, world!')
+    response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
